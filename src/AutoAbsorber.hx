f488ac3927a8ad5ab6419414ea4cb8083081dde4
Tomosuke.St <tmskst@gmail.com>
2014-04-22 03:06:09 +0900

    マクロを更新

--- src/AutoAbsorber.hx
+++ src/AutoAbsorber.hx
@@ -16,12 +16,8 @@ class Absorber {
 		/* AST */
 		var fields = Context.getBuildFields();
 		
-		/* 対象のメタデータ名 */
-		var METADATA_NAME_ABSORB = ":absorb";
-		var METADATA_NAME_ABSORB_KEY = ":absorbKey";
-		
 		/* 対象の構造体を回収するためのリスト */
-		var absorbKeyList:List<{ mpcn:String, mpf:String, vtn:String, fn:String}> = new List();
+		var absorbKeyList:List<{ mpcn:String, mpf:String, fn:String}> = new List();
 		var absorbList:List<{ vtn:String, fn:String }> = new List();
 		
 		// すべてのフィールドを走査して
@@ -33,21 +29,21 @@ class Absorber {
 					for (meta in field_meta) {
 						switch (meta) {
 							// 対象のメタデータが '@:absorbKey' ならば
-							case { name : meta_name } if (meta_name == METADATA_NAME_ABSORB_KEY) :
+							case { name : _ => ":absorbKey" } :
 								// FIXME : メタデータのパラメータは必ず1つであるから走査する必要がない
 								// メタデータのすべてのパラメータを走査して
 								for (param in meta.params) {
 									switch (param) {
 										// パラメータの構文解析 : <meta_param_cident_name:Enum>.<meta_param_field:EnumValue>
 										case { expr : ExprDef.EField( { expr : ExprDef.EConst(Constant.CIdent(meta_param_cident_name)) }, meta_param_field) } :
-											/* trace(meta_name, meta_param_cident_name, meta_param_field, var_tpath_name, field_name); */
-											absorbKeyList.add( { mpcn : meta_param_cident_name, mpf : meta_param_field, vtn : var_tpath_name, fn : field_name });
+											/* trace(meta_param_cident_name, meta_param_field, var_tpath_name, field_name); */
+											absorbKeyList.add( { mpcn : meta_param_cident_name, mpf : meta_param_field, fn : field_name });
 										case _ : 
 											Context.warning("#", Context.currentPos());
 									}
 								}
 							// 対象のメタデータが '@:absorb' ならば
-							case { name : meta_name } if (meta_name == METADATA_NAME_ABSORB) :
+							case { name : _ => ":absorb" } :
 								/* trace(meta_name, var_tpath_name, field_name); */
 								absorbList.add( { vtn : var_tpath_name, fn : field_name, });
 						}
@@ -74,7 +70,7 @@ class Absorber {
 		/* absorberをASTに挿入する */
 		for (field in fields) {
 			switch (field) {
-				case { kind : FieldType.FFun( { expr : { expr : ExprDef.EBlock(ffun_exprs) } } ), name: field_name } if (field_name == "new") : 
+				case { kind : FieldType.FFun( { expr : { expr : ExprDef.EBlock(ffun_exprs) } } ), name: _ => "new" } : 
 					// FIXME : 何も考えずにASTをコンストラクタの2コンテキスト目に挿入しているので処理としては不適切
 					ffun_exprs.insert(1, absorber);
 				case _ :
