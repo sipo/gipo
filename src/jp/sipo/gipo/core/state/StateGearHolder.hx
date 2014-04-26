07c560c4aedc65c838713490c52a8320232fa960
sipo <github@sipo.jp>
2014-04-26 10:35:06 +0900

    StateGearHolderに説明を追加

--- src/jp/sipo/gipo/core/state/StateGearHolder.hx
+++ src/jp/sipo/gipo/core/state/StateGearHolder.hx
@@ -1,4 +1,10 @@
 package jp.sipo.gipo.core.state;
+/**
+ * StateSwitcherで切り替える対象となれるインターフェース
+ * ゲームの遷移画面や、ロード状態など、処理の切り替わりに利用する。
+ * 
+ * @author sipo
+ */
 interface StateGearHolder extends GearHolder
 {
 	function getStateGear():StateGear;
--- src/jp/sipo/gipo/core/state/StateGearHolderImpl.hx
+++ src/jp/sipo/gipo/core/state/StateGearHolderImpl.hx
@@ -1,4 +1,10 @@
 package jp.sipo.gipo.core.state;
+/**
+ * StateSwitcherで切り替える対象となるクラス
+ * ゲームの遷移画面や、ロード状態など、処理の切り替わりに利用する。
+ * 
+ * @author sipo
+ */
 class StateGearHolderImpl extends GearHolderImpl implements StateGearHolder
 {
 	private var stateGear:StateGear;
