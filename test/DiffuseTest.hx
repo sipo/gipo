fa474254d14e1e8024d5d53204ece6cdcd71aad3
lllllT <lllllT@tamanegi.org>
2015-01-21 14:39:59 +0900

    add tests for diffuse

--- test/DiffuseTest.hx
+++ test/DiffuseTest.hx
@@ -13,6 +13,8 @@ class DiffuseTest
 
 	/*
 	 * top -- nodeA -- nodeAA
+	 *
+	 * top diffuses Data.
 	 */
 	@Test
 	public function testDiffuseStraight():Void
@@ -42,6 +44,8 @@ class DiffuseTest
 	/*
 	 * top -- nodeA -- nodeAA
 	 *     `- nodeB -- nodeBB
+	 *
+	 * top diffuses Data.
 	 */
 	@Test
 	public function testDiffuseFork():Void
@@ -76,6 +80,225 @@ class DiffuseTest
 		Assert.areEqual(data, nodeB.manualData);
 		Assert.areEqual(data, nodeBB.autoData);
 		Assert.areEqual(data, nodeBB.manualData);
+	}
+
+	/*
+	 * top -- nodeA -- nodeAA
+	 *     `- nodeB -- nodeBB
+	 *
+	 * top diffuses Data.
+	 * nodeA diffuses another Data.
+	 */
+	@Test
+	public function testDiffusePartial():Void
+	{
+		var data = new Data();
+		var dataA = new Data();
+
+		var top = new TreeInfo();
+		top.diffuseData = data;
+
+		var nodeA = new TreeInfo();
+		nodeA.diffuseData = dataA;
+		top.children.push(nodeA);
+
+		var nodeAA = new TreeInfo();
+		nodeA.children.push(nodeAA);
+
+		var nodeB = new TreeInfo();
+		top.children.push(nodeB);
+
+		var nodeBB = new TreeInfo();
+		nodeB.children.push(nodeBB);
+
+		// build gear tree
+		var top = new Top(top);
+		top.gearOutside().initializeTop(null);
+
+		// verify
+		Assert.areEqual(data, nodeA.autoData); // absorbed before diffuse dataA
+		Assert.areEqual(dataA, nodeA.manualData); // absorbed after diffuse dataA
+		Assert.areEqual(dataA, nodeAA.autoData);
+		Assert.areEqual(dataA, nodeAA.manualData);
+		Assert.areEqual(data, nodeB.autoData);
+		Assert.areEqual(data, nodeB.manualData);
+		Assert.areEqual(data, nodeBB.autoData);
+		Assert.areEqual(data, nodeBB.manualData);
+	}
+
+	/*
+	 * top -- nodeA -- nodeAA
+	 *
+	 * top diffuses DataSub.
+	 */
+	@Test
+	public function testDiffuseSubclass():Void
+	{
+		var data = new DataSub();
+
+		var top = new TreeInfo();
+		top.diffuseData = data;
+
+		var nodeA = new TreeInfo();
+		top.children.push(nodeA);
+
+		var nodeAA = new TreeInfo();
+		nodeA.children.push(nodeAA);
+
+		// build gear tree
+		var top = new Top(top);
+		top.gearOutside().initializeTop(null);
+
+		// verify
+		Assert.areEqual(data, nodeA.autoData);
+		Assert.areEqual(data, nodeA.manualData);
+		Assert.areEqual(data, nodeAA.autoData);
+		Assert.areEqual(data, nodeAA.manualData);
+	}
+
+	/*
+	 * top -- nodeA -- nodeAA
+	 *
+	 * top diffuses Data with key.
+	 */
+	@Test
+	public function testDiffuseStraightWithKey():Void
+	{
+		var data = new Data();
+
+		var top = new TreeInfo();
+		top.diffuseData = data;
+
+		var nodeA = new TreeInfo();
+		top.children.push(nodeA);
+
+		var nodeAA = new TreeInfo();
+		nodeA.children.push(nodeAA);
+
+		// build gear tree
+		var top = new TopWithKey(top);
+		top.gearOutside().initializeTop(null);
+
+		// verify
+		Assert.areEqual(data, nodeA.autoData);
+		Assert.areEqual(data, nodeA.manualData);
+		Assert.areEqual(data, nodeAA.autoData);
+		Assert.areEqual(data, nodeAA.manualData);
+	}
+
+	/*
+	 * top -- nodeA -- nodeAA
+	 *     `- nodeB -- nodeBB
+	 *
+	 * top diffuses Data with key.
+	 */
+	@Test
+	public function testDiffuseForkWithKey():Void
+	{
+		var data = new Data();
+
+		var top = new TreeInfo();
+		top.diffuseData = data;
+
+		var nodeA = new TreeInfo();
+		top.children.push(nodeA);
+
+		var nodeAA = new TreeInfo();
+		nodeA.children.push(nodeAA);
+
+		var nodeB = new TreeInfo();
+		top.children.push(nodeB);
+
+		var nodeBB = new TreeInfo();
+		nodeB.children.push(nodeBB);
+
+		// build gear tree
+		var top = new TopWithKey(top);
+		top.gearOutside().initializeTop(null);
+
+		// verify
+		Assert.areEqual(data, nodeA.autoData);
+		Assert.areEqual(data, nodeA.manualData);
+		Assert.areEqual(data, nodeAA.autoData);
+		Assert.areEqual(data, nodeAA.manualData);
+		Assert.areEqual(data, nodeB.autoData);
+		Assert.areEqual(data, nodeB.manualData);
+		Assert.areEqual(data, nodeBB.autoData);
+		Assert.areEqual(data, nodeBB.manualData);
+	}
+
+	/*
+	 * top -- nodeA -- nodeAA
+	 *     `- nodeB -- nodeBB
+	 *
+	 * top diffuses Data.
+	 * nodeA diffuses another Data with key.
+	 */
+	@Test
+	public function testDiffusePartialWithKey():Void
+	{
+		var data = new Data();
+		var dataA = new Data();
+
+		var top = new TreeInfo();
+		top.diffuseData = data;
+
+		var nodeA = new TreeInfo();
+		nodeA.diffuseData = dataA;
+		top.children.push(nodeA);
+
+		var nodeAA = new TreeInfo();
+		nodeA.children.push(nodeAA);
+
+		var nodeB = new TreeInfo();
+		top.children.push(nodeB);
+
+		var nodeBB = new TreeInfo();
+		nodeB.children.push(nodeBB);
+
+		// build gear tree
+		var top = new TopWithKey(top);
+		top.gearOutside().initializeTop(null);
+
+		// verify
+		Assert.areEqual(data, nodeA.autoData); // absorbed before diffuse dataA
+		Assert.areEqual(dataA, nodeA.manualData); // absorbed after diffuse dataA
+		Assert.areEqual(dataA, nodeAA.autoData);
+		Assert.areEqual(dataA, nodeAA.manualData);
+		Assert.areEqual(data, nodeB.autoData);
+		Assert.areEqual(data, nodeB.manualData);
+		Assert.areEqual(data, nodeBB.autoData);
+		Assert.areEqual(data, nodeBB.manualData);
+	}
+
+	/*
+	 * top -- nodeA -- nodeAA
+	 *
+	 * top diffuses DataSub with key.
+	 */
+	@Test
+	public function testDiffuseSubclassWithKey():Void
+	{
+		var data = new DataSub();
+
+		var top = new TreeInfo();
+		top.diffuseData = data;
+
+		var nodeA = new TreeInfo();
+		top.children.push(nodeA);
+
+		var nodeAA = new TreeInfo();
+		nodeA.children.push(nodeAA);
+
+		// build gear tree
+		var top = new TopWithKey(top);
+		top.gearOutside().initializeTop(null);
+
+		// verify
+		Assert.areEqual(data, nodeA.autoData);
+		Assert.areEqual(data, nodeA.manualData);
+		Assert.areEqual(data, nodeAA.autoData);
+		Assert.areEqual(data, nodeAA.manualData);
 	}
 }
 
@@ -120,6 +343,47 @@ class Node extends Top
 	}
 }
 
+class TopWithKey extends GearHolderImpl
+{
+	var info:TreeInfo;
+
+	public function new(info:TreeInfo)
+	{
+		super();
+		this.info = info;
+	}
+
+	@:handler(GearDispatcherKind.Diffusible)
+	function diffusible(tool:GearDiffuseTool):Void
+	{
+		if(info.diffuseData != null) {
+			tool.diffuseWithKey(info.diffuseData, DataKey.Key1);
+		}
+
+		for(child in info.children) {
+			tool.bookChild(new NodeWithKey(child));
+		}
+	}
+}
+
+class NodeWithKey extends TopWithKey
+{
+	@:absorbWithKey(DataKey.Key1)
+	public var autoData:Data;
+
+	public function new(info:TreeInfo)
+	{
+		super(info);
+	}
+
+	@:handler(GearDispatcherKind.Run)
+	function run():Void
+	{
+		info.autoData = autoData;
+		info.manualData = gear.absorbWithKey(DataKey.Key1);
+	}
+}
+
 class TreeInfo
 {
 	public var children:Array<TreeInfo>;
@@ -137,4 +401,15 @@ class TreeInfo
 class Data
 {
 	public function new() {}
+}
+
+class DataSub extends Data
+{
+	public function new() { super(); }
+}
+
+enum DataKey
+{
+	Key1;
+	Key2;
 }
